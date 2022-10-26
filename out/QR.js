9561af1012a152c68b72e0ddabe48d528637fe24
cajunwildcat <elicheramie89@gmail.com>
2022-10-26 16:20:52 -0400

    add format strings

--- out/QR.js
+++ out/QR.js
@@ -9,67 +9,76 @@ var OrientationAnchor = [
     [1, 1, 1, 1, 1, 1, 1, 0],
     [0, 0, 0, 0, 0, 0, 0, 0]
 ];
+//<==================================>
 //constructs the base of a QR code of version V, 
 //which includes the orientation anchors, the timing patterns, and the dark module
 var constructQRBase = function (V) {
     var size = ((V - 1) * 4) + 21;
-    var QR = [];
+    var qr = {
+        code: [],
+        version: V,
+        addFormat: addQRFormat
+    };
     for (var row = 0; row < size; row++) {
-        QR[row] = [];
+        qr.code[row] = [];
         for (var column = 0; column < size; column++) {
             //top left anchor
             if (row < OrientationAnchor.length && column < OrientationAnchor.length) {
-                QR[row][column] = OrientationAnchor[column][row];
+                qr.code[row][column] = OrientationAnchor[column][row];
             }
             //top right anchor
             else if (row < OrientationAnchor.length && column > size - 9) {
-                QR[row][column] = OrientationAnchor[Math.abs(column - (size - 1))][row];
+                qr.code[row][column] = OrientationAnchor[Math.abs(column - (size - 1))][row];
             }
             else if (row > size - 9 && column < OrientationAnchor.length) {
-                QR[row][column] = OrientationAnchor[column][Math.abs(row - (size - 1))];
+                qr.code[row][column] = OrientationAnchor[column][Math.abs(row - (size - 1))];
             }
             //horizontal timing
             else if (row === 6) {
                 if (column % 2 === 0) {
-                    QR[row][column] = 1;
+                    qr.code[row][column] = 1;
                 }
                 else {
-                    QR[row][column] = 0;
+                    qr.code[row][column] = 0;
                 }
             }
             //vertical timing
             else if (column === 6) {
                 if (row % 2 === 0) {
-                    QR[row][column] = 1;
+                    qr.code[row][column] = 1;
                 }
                 else {
-                    QR[row][column] = 0;
+                    qr.code[row][column] = 0;
                 }
             }
             //fill the rest of the area with empty values
             else {
-                QR[row][column] = 0;
+                qr.code[row][column] = 0;
             }
         }
     }
     //dark module
-    QR[V * 4 + 9][8] = 1;
-    return QR;
+    qr.code[V * 4 + 9][8] = 1;
+    return qr;
 };
+var addQRFormat = function (ecl, mask) {
+    var newQR;
+    return newQR;
+};
+//<===========================>
 //converts a binary representation of a QR code into a string representation
 //using the full block unicode character (u2588) and spaces
 var convertToBlocks = function (qr) {
     var newQR = [];
     //convert each row
-    for (var i = 0; i < qr.length; i++) {
+    for (var i = 0; i < qr.code.length; i++) {
         //map 1->block 0->[space] then combine them into a single string with no characters between
-        var temp = qr[i].map(function (n) { return n === 1 ? '\u2588' : ' '; }).join('');
+        var temp = qr.code[i].map(function (n) { return n === 1 ? '\u2588' : ' '; }).join('');
         newQR[i] = temp;
     }
     return newQR;
 };
 var V1 = convertToBlocks(constructQRBase(1));
 var V5 = convertToBlocks(constructQRBase(5));
-V5.forEach(function (s) { return console.log(s); });
-debugger;
+V1.forEach(function (s) { return console.log(s); });
 //# sourceMappingURL=QR.js.map
--- src/QR.ts
+++ src/QR.ts
@@ -10,70 +10,154 @@ const OrientationAnchor: number[][] = [
     [0, 0, 0, 0, 0, 0, 0, 0]
 ];
 
+const formats: number[] = [
+    111011111000100,
+    111001011110011,
+    111110110101010,
+    111100010011101,
+    110011000101111,
+    110001100011000,
+    110110001000001,
+    110100101110110,
+    101010000010010,
+    101000100100101,
+    101111001111100,
+    101101101001011,
+    100010111111001,
+    100000011001110,
+    100111110010111,
+    100101010100000,
+    0o011010101011111,
+    0o011000001101000,
+    0o011111100110001,
+    0o011101000000110,
+    0o010010010110100,
+    0o010000110000011,
+    0o010111011011010,
+    0o010101111101101,
+    0o001011010001001,
+    0o001001110111110,
+    0o001110011100111,
+    0o001100111010000,
+    0o000011101100010,
+    0o000001001010101,
+    0o000110100001100,
+    0o000100000111011
+];
+const QRFormats = new Uint16Array(formats);
+
+type QR = {
+    code: number[][],
+    version: number,
+
+    addFormat: (ECL, MaskPattern) => QR,
+}
+type ECL = 'L' | 'M' | 'Q' | 'H';
+type MaskPattern = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7;
+
+//<==================================>
+
 //constructs the base of a QR code of version V, 
 //which includes the orientation anchors, the timing patterns, and the dark module
-const constructQRBase = (V: number): number[][] => {
+const QR = (V: number): QR => {
     const size = ((V - 1) * 4) + 21;
-    let QR: number[][] = [];
 
+    let newCode: number[][] = [];
     for (let row = 0; row < size; row++) {
-        QR[row] = [];
+        newCode[row] = [];
 
         for (let column = 0; column < size; column++) {
             //top left anchor
             if (row < OrientationAnchor.length && column < OrientationAnchor.length) {
-                QR[row][column] = OrientationAnchor[column][row];
+                newCode[row][column] = OrientationAnchor[column][row];
             }
 
             //top right anchor
             else if (row < OrientationAnchor.length && column > size - 9) {
-                QR[row][column] = OrientationAnchor[Math.abs(column - (size - 1))][row];
+                newCode[row][column] = OrientationAnchor[Math.abs(column - (size - 1))][row];
             }
 
             else if (row > size - 9 && column < OrientationAnchor.length) {
-                QR[row][column] = OrientationAnchor[column][Math.abs(row - (size - 1))];
+                newCode[row][column] = OrientationAnchor[column][Math.abs(row - (size - 1))];
             }
 
             //horizontal timing
             else if (row === 6) {
                 if (column % 2 === 0) {
-                    QR[row][column] = 1;
+                    newCode[row][column] = 1;
                 }
                 else {
-                    QR[row][column] = 0;
+                    newCode[row][column] = 0;
                 }
             }
 
             //vertical timing
             else if (column === 6) {
                 if (row % 2 === 0) {
-                    QR[row][column] = 1;
+                    newCode[row][column] = 1;
                 }
                 else {
-                    QR[row][column] = 0;
+                    newCode[row][column] = 0;
                 }
             }
 
             //fill the rest of the area with empty values
             else {
-                QR[row][column] = 0;
+                newCode[row][column] = 0;
             }
         }
     }
     //dark module
-    QR[V * 4 + 9][8] = 1;
+    newCode[V * 4 + 9][8] = 1;
 
-    return QR;
+    return {
+        version: V,
+        code: newCode,
+
+        addFormat: (ecl: ECL, mask: MaskPattern): QR => {
+            //generate index based on ecl and mask to get correct format bits from lookup array
+            let formatBits: number;
+            let formatIndex: number = 0;
+            switch (ecl) {
+                case 'H':
+                    formatIndex += 8;
+                case 'Q':
+                    formatIndex += 8;
+                case 'M':
+                    formatIndex += 8;
+                    break;
+            }
+            formatIndex += mask;
+            formatBits = QRFormats[formatIndex];
+
+            //fill in the QR code with the correct bits
+            let newCode = this.code;
+
+            for (let column = 0; column <= 8; column++) {
+                //start of timing pattern, don't touch that bit
+                if (column === 6) {
+                    continue;
+                }
+                newCode[8][column] = QRFormats[column];
+                //bottom left anchor repeat
+                if (column === 8) {
+
+                }
+            }
+        }
+    }
 }
+
+//<===========================>
 
 //converts a binary representation of a QR code into a string representation
 //using the full block unicode character (u2588) and spaces
-const convertToBlocks = (qr: number[][]): string[] => {
+const convertToBlocks = (qr: QR): string[] => {
     let newQR: string[] = [];
     //convert each row
-    for (let i = 0; i < qr.length; i++) {
+    for (let i = 0; i < qr.code.length; i++) {
         //map 1->block 0->[space] then combine them into a single string with no characters between
-        let temp: string = qr[i].map((n) => n === 1 ? '\u2588' : ' ').join('');
+        let temp: string = qr.code[i].map((n) => n === 1 ? '\u2588' : ' ').join('');
         newQR[i] = temp;
     }
 
@@ -82,6 +166,4 @@ const convertToBlocks = (qr: number[][]): string[] => {
 
 const V1 = convertToBlocks(constructQRBase(1));
 const V5 = convertToBlocks(constructQRBase(5));
-V5.forEach((s: string) => console.log(s));
-
-debugger;
+V1.forEach((s: string) => console.log(s));
